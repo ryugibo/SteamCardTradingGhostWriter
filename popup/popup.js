2a5fbbaa312eb2ace4f0042b0d309e7914748eca
ryugibo <7928632+ryugibo@users.noreply.github.com>
2025-01-03 11:13:40 +0900

    버튼 클릭 없이 확장 팝업 열면 처리

--- popup/popup.js
+++ popup/popup.js
@@ -1,34 +1,30 @@
-document.getElementById("btnSave").addEventListener(
-  "click",
-  () => {
-    var query = { active: true, currentWindow: true };
-    async function callback(tabs) {
-      var currentTab = tabs[0]; // there will be only one in this array
-      const response = await chrome.tabs.sendMessage(currentTab.id, {
-        greeting: "hello",
-      });
+window.onload = function () {
+  var query = { active: true, currentWindow: true };
+  async function callback(tabs) {
+    var currentTab = tabs[0]; // there will be only one in this array
+    const response = await chrome.tabs.sendMessage(currentTab.id, {
+      greeting: "hello",
+    });
 
-      console.log(response);
-      const have_cards = response["have"];
-      const want_cards = response["want"];
+    console.log(response);
+    const have_cards = response["have"];
+    const want_cards = response["want"];
 
-      const haveCardsString = have_cards.map(({ index }) => index).join(", ");
-      const wantCardsString = want_cards.map(({ index }) => index).join(", ");
-      document.getElementById("txtTitle").value =
-        "1:1 [H] " + haveCardsString + " [W] " + wantCardsString;
+    const haveCardsString = have_cards.map(({ index }) => index).join(", ");
+    const wantCardsString = want_cards.map(({ index }) => index).join(", ");
+    document.getElementById("txtTitle").value =
+      "1:1 [H] " + haveCardsString + " [W] " + wantCardsString;
 
-      document.getElementById("txtContents").value = [
-        "[h1][HAVE][/h1]",
-        "[list]",
-        have_cards.map(({ name }) => `[*] ${name}`).join("\n"),
-        "[/list]",
-        "[h1][WANT][/h1]",
-        "[list]",
-        want_cards.map(({ name }) => `[*] ${name}`).join("\n"),
-        "[/list]",
-      ].join("\n");
-    }
-    chrome.tabs.query(query, callback);
-  },
-  false
-);
+    document.getElementById("txtContents").value = [
+      "[h1][HAVE][/h1]",
+      "[list]",
+      have_cards.map(({ name }) => `[*] ${name}`).join("\n"),
+      "[/list]",
+      "[h1][WANT][/h1]",
+      "[list]",
+      want_cards.map(({ name }) => `[*] ${name}`).join("\n"),
+      "[/list]",
+    ].join("\n");
+  }
+  chrome.tabs.query(query, callback);
+};
